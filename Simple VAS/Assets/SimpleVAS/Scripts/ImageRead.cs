96259e9d466bf3f691c0067ab448d81f35fe12ef
marteroel <marteroel@users.noreply.github.com>
2020-02-13 17:07:22 +0100

    Added basic image forced choice / likert interactions, yet to add shuffling to whole project

---
+++ Simple VAS/Assets/SimpleVAS/Scripts/ImageRead.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+namespace SimpleVAS { 
+    public class ImageRead : MonoBehaviour {
+
+	    public static List<Sprite> imageSprites = new List<Sprite>();
+
+        // Use this for initialization
+	    void Start () {
+
+            string[] filePaths = Directory.GetFiles("./Images/", "*.jpg");
+
+            foreach(string path in filePaths) {
+                Texture2D spriteTexture = LoadTexture(path);
+                imageSprites.Add(Sprite.Create(spriteTexture, new Rect(0, 0, spriteTexture.width, spriteTexture.height), new Vector2(0, 0), 100f, 0, SpriteMeshType.Tight));
+            }
+        
+        }
+
+        public Texture2D LoadTexture(string FilePath) //adapted from https://forum.unity.com/threads/generating-sprites-dynamically-from-png-or-jpeg-files-in-c.343735/
+        {
+            // Load a PNG or JPG file from disk to a Texture2D
+            Texture2D _tex2D;
+            byte[] _fileData;
+
+            if (File.Exists(FilePath)) {
+                _fileData = File.ReadAllBytes(FilePath);
+                _tex2D = new Texture2D(2, 2);           // Create new "empty" texture
+                if (_tex2D.LoadImage(_fileData))           // Load the imagedata into the texture (size is set automatically)
+                    return _tex2D;                 // If data = readable -> return texture
+            }
+            return null;                     // Return null if load failed
+        }
+
+    }
+
+
+}
---
+++ Simple VAS/Assets/SimpleVAS/Scripts/QuestionManagerImages.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace SimpleVAS
+{
+    public class QuestionManagerImages : MonoBehaviour
+    {
+
+        List<Sprite> questionList = new List<Sprite>();
+
+        public Image _image;
+        public Button nextButton;
+
+        public ToggleGroup toggleGroup;
+
+        public CsvWrite csvWriter;
+
+        private int currentItem;
+
+        // Use this for initialization
+        void Start()
+        {
+
+            currentItem = 0;
+            questionList = ImageRead.imageSprites;
+            _image.sprite = questionList[currentItem];
+            nextButton.interactable = false;
+
+        }
+
+        public void OnToggleSelection()
+        {
+            nextButton.interactable = true;
+        }
+
+
+        public void OnNextButton()
+        {
+
+            Toggle[] numberOfToggles = toggleGroup.GetComponentsInChildren<Toggle>();
+
+            for (int i = 0; i < numberOfToggles.Length; i++)
+            {
+                if (numberOfToggles[i].isOn)
+                {
+                    QuestionManager.ResponseValue = i.ToString();
+                }
+            }
+
+
+            nextButton.interactable = false;
+            QuestionManager.questionnaireItem = currentItem.ToString();
+            csvWriter.onNextButtonPressed();
+
+            toggleGroup.SetAllTogglesOff();
+
+            currentItem++;
+
+            if (currentItem < questionList.Count)
+                _image.sprite = questionList[currentItem];
+
+
+            else if (currentItem == questionList.Count)
+            {
+                currentItem = 0;
+                questionList.Clear();
+
+                SceneManager.LoadScene("VAS");
+            }
+        }
+    }
+}
