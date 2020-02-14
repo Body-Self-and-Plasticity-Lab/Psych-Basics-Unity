e263e63d0c8fd27813575aa2a8b1160a82da2e4c
marteroel <marteroel@users.noreply.github.com>
2020-02-14 14:39:02 +0100

    added shuffle to all tasks

--- Simple VAS/Assets/SimpleVAS/Scripts/ForcedChoiceManager.cs
+++ Simple VAS/Assets/SimpleVAS/Scripts/ForcedChoiceManager.cs
@@ -9,8 +9,8 @@ namespace SimpleVAS
 {
     public class ForcedChoiceManager : MonoBehaviour
     {
-
         List<string> questionList = new List<string>();
+        private List<int> indexList = new List<int>();
 
         public Text questionUI;
         public Button nextButton;
@@ -20,19 +20,37 @@ namespace SimpleVAS
         public CsvWrite csvWriter;
 
         private int currentItem;
+        public bool shuffle;
 
         // Use this for initialization
         void Start() {
 
-            currentItem = 0;
             questionList = CsvRead.questionnaireInput;
+
+            if (!shuffle)
+                currentItem = 0;
+
+            else {
+                for (int i = 0; i < questionList.Count; i++)
+                    indexList.Add(i);
+
+                currentItem = ShuffleValue();
+            }
+           
             questionUI.text = questionList[currentItem];
             nextButton.interactable = false;
-
         }
 
         public void OnToggleSelection() {
             nextButton.interactable = true;
+        }
+
+        private int ShuffleValue() {
+            int randomIndex = Random.Range(0, indexList.Count);
+            int selectedItem = indexList[randomIndex];
+            indexList.RemoveAt(randomIndex);
+
+            return selectedItem;
         }
 
 
@@ -53,21 +71,36 @@ namespace SimpleVAS
             QuestionManager.questionnaireItem = currentItem.ToString();
             csvWriter.onNextButtonPressed();
 
-            toggleGroup.SetAllTogglesOff();
+            toggleGroup.SetAllTogglesOff();       
 
-            currentItem++;
+            if (!shuffle) {
+                currentItem++;
 
-            if (currentItem < questionList.Count)
-                questionUI.text = questionList[currentItem];
+                if (currentItem < questionList.Count)
+                    questionUI.text = questionList[currentItem];
 
-
-            else if (currentItem == questionList.Count)
-            {
-                currentItem = 0;
-                questionList.Clear();
-
-                SceneManager.LoadScene("VAS");
+                else if (currentItem == questionList.Count)
+                    QuestionsExhausted();
             }
+
+            else {
+
+                if (indexList.Count != 0) {
+                    currentItem = ShuffleValue();
+                    questionUI.text = questionList[currentItem];
+                }
+
+                else if (indexList.Count == 0)
+                    QuestionsExhausted();
+            }
+        }
+
+        private void QuestionsExhausted()
+        {
+            currentItem = 0;
+            questionList.Clear();
+
+            SceneManager.LoadScene("VAS");
         }
     }
 }
