9a268a168b66f5ffc16f64136bd6fe51ef8c7c8b
marteroel <marteroel@users.noreply.github.com>
2020-02-25 14:35:54 +0100

    Test to create toggles dynamically

---
+++ Simple VAS/Assets/SimpleVAS/Scripts/ScaleSettings.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityPsychBasics
+{
+    public class ScaleSettings : MonoBehaviour {
+
+        public GameObject togglePrefab;
+        public Transform toggleParent;
+
+        public string scaleMin, scaleMid, scaleMax;
+
+        public Text minText, midText, maxText;
+
+        public int likertSize;
+
+        private TaskManager _taskManager;
+
+        private void Start() {
+
+            _taskManager = TaskManager.instance;
+
+            for (int i = 1; i <= likertSize; i++)
+                CreateToggle();
+        }
+
+        private void CreateToggle() {
+
+            GameObject newToggle = Instantiate(togglePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+
+            newToggle.transform.SetParent(toggleParent, false);
+
+            //newToggle = toggleGroup;
+
+            Toggle toggleBehavior = newToggle.GetComponent<Toggle>();
+
+            toggleBehavior.onValueChanged.AddListener(delegate { ToggleAction(toggleBehavior); });
+
+        }
+        
+        private void ToggleAction(Toggle changed){
+            Debug.Log("toggle pressed");
+            _taskManager.OnResponseSelection();
+        }
+    }
+}
