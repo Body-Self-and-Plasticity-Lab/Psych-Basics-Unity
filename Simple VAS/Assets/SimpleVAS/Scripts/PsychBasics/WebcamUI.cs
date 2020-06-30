5eaa18b9d0390c88a581da530a1ddd1887b4307a
marteroel <marteroel@users.noreply.github.com>
2020-06-30 14:51:13 +0200

    Added webcam support

---
+++ Simple VAS/Assets/SimpleVAS/Scripts/PsychBasics/WebcamUI.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityPsychBasics
+{
+    public class WebcamUI : MonoBehaviour {
+
+	    public RawImage display;
+	    public Dropdown webcamSelector;
+
+	    private WebCamTexture webcamTexture;
+
+	    // Use this for initialization
+	    void Start () {
+		    webcamSelector.value = PlayerPrefs.GetInt ("selectedCamera");
+		    SetWebCamOptions ();
+		    SelectWebcam ();
+	    }
+
+	    public void SetWebCamOptions(){
+		    webcamSelector.options.Clear ();
+
+		    List<string> devices = new List<string> ();
+
+		    foreach (var item in WebCamTexture.devices) {
+			    devices.Add(item.name);
+		    }
+
+		    webcamSelector.AddOptions (devices);
+	    }
+
+	    public void SelectWebcam () {
+
+		    if(webcamTexture != null) webcamTexture.Stop();
+
+		    WebCamDevice[] devices = WebCamTexture.devices;
+		    string deviceName = devices[webcamSelector.value].name;
+		    webcamTexture = new WebCamTexture(deviceName);
+
+		    display.texture = webcamTexture;
+
+		    webcamTexture.Play();
+	    }
+
+
+	    void OnDestroy(){
+		    PlayerPrefs.SetInt ("selectedCamera", webcamSelector.value);
+		    if(webcamTexture != null) webcamTexture.Stop();
+	    }
+    }
+}
