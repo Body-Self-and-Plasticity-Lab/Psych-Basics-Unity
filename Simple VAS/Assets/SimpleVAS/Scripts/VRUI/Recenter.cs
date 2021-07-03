aea22ec5f56ee3fbd503b368cce8dc938ebaacd8
Marte Roel <marteroel@users.noreply.github.com>
2021-07-03 15:04:14 -0500

    Build

---
+++ Simple VAS/Assets/SimpleVAS/Scripts/VRUI/Recenter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Recenter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        InputTracking.disablePositionalTracking = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("c"))
+        {
+            InputTracking.Recenter();
+        }
+	}
+}
